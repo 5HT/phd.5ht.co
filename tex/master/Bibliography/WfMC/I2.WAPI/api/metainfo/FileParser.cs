356d95befb587c89593759e459b8a1cc74f85fcf
tonpa <maxim@synrc.com>
2021-05-31 15:32:08 +0300

    msc

---
+++ tex/master/Bibliography/WfMC/I2.WAPI/api/metainfo/FileParser.cs
@@ -0,0 +1,16 @@
+
+public namespace ILS.Workflow {
+
+	public class FileParser {
+
+		public FileParser {
+
+			RegisterElement("", typeof());
+		}
+
+		public class RegisterElement(string name, Type entity) {
+		}
+
+	}
+
+}
